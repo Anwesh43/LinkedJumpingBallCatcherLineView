b44d51343e5971c47d676f867536d4e056d3220f
Anwesh43 <anweshthecool0@gmail.com>
2020-05-18 04:59:16 +0530

    adding JBCLNode class

--- jumpingballcatcherview/src/main/java/com/anwesh/uiprojects/jumpingballcatcherview/JumpingBallCatcherLineView.kt
+++ jumpingballcatcherview/src/main/java/com/anwesh/uiprojects/jumpingballcatcherview/JumpingBallCatcherLineView.kt
@@ -116,4 +116,45 @@ class JumpingBallCatcherLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JBCLNode(var i : Int, val state : State = State()) {
+
+        private var next : JBCLNode? = null
+        private var prev : JBCLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = JBCLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJBCLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : JBCLNode {
+            var curr : JBCLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
