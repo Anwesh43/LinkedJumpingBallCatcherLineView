a949a2f71f0a77c0e13e51fae3cd90af6ce7fe0b
Anwesh43 <anweshthecool0@gmail.com>
2020-05-18 04:47:20 +0530

    adding Canvas extension method

--- jumpingballcatcherview/src/main/java/com/anwesh/uiprojects/jumpingballcatcherview/JumpingBallCatcherLineView.kt
+++ jumpingballcatcherview/src/main/java/com/anwesh/uiprojects/jumpingballcatcherview/JumpingBallCatcherLineView.kt
@@ -15,9 +15,9 @@ import android.content.Context
 val nodes : Int = 5
 val lines : Int = 2
 val parts : Int = 3
-val scGap : Float = 0.02f / (parts - 1)
+val scGap : Float = 0.02f / parts
 val strokeFactor : Float = 90f
-val sizeFactor : Float = 4f
+val sizeFactor : Float = 7.8f
 val colors : Array<String> = arrayOf("#673AB7", "#4CAF50", "#F44336", "#3F51B5", "#009688")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
@@ -26,3 +26,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawJumpingBallCatcherLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val ballSize : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    save()
+    translate(0f, h / 2)
+    drawCircle(0f, -ballSize + h * sf2, ballSize, paint)
+    for (j in 0..(lines - 1)) {
+        val x1 : Float = w / 2 * (1 + sf1) * j
+        val x2 : Float = w * j + (1 - j) * sf1 * (w / 2)
+        drawLine(x1, 0f, x2, 0f, paint)
+    }
+    restore()
+}
+
+fun Canvas.drawJBCLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    drawJumpingBallCatcherLine(scale, w, h, paint)
+    restore()
+}
